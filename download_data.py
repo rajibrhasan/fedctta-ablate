8efbec094680a9b2364eb637517c6133fd70e43f
rajibrhasan <rajib.rhasan@gmail.com>
2025-01-29 11:12:33 +0600

    Updated configs for ablation

--- download_data.py
+++ download_data.py
@@ -5,13 +5,13 @@ import gdown
 
 
 x_test, y_tes = load_cifar10c(10000, data_dir = 'data/CIFAR10')
-x_test, y_test = load_cifar100c(10000, data_dir='data/CIFAR100')
+x_test, y_test = load_cifar100c(10000, data_dir = 'data/CIFAR100')
 
-model =  load_model('Hendrycks2020AugMix_ResNeXt', 'pretrain', 'cifar100', 'corruptions')
-model = load_model(model_name='Wang2023Better_WRN-28-10', dataset='cifar10', threat_model='Linf')
+model =  load_model(model_name = 'Hendrycks2020AugMix_ResNeXt', dataset =  'cifar100', threat_model = 'corruptions')
+# model = load_model(model_name='Wang2023Better_WRN-28-10', dataset='cifar10', threat_model='Linf')
+# url = "https://drive.google.com/file/d/1Xy6kVJ8d27RpfE2t8sPBuczHtrS-ZWwP/view?usp=sharing"
 
-url = "https://drive.google.com/file/d/1Xy6kVJ8d27RpfE2t8sPBuczHtrS-ZWwP/view?usp=sharing"
-id = "1Xy6kVJ8d27RpfE2t8sPBuczHtrS-ZWwP"
+id = "1eJu4wBxzeqf4INlOkuBr9Co1NTG4wIVG"
 output = "./pretrain/resnet8_cifar10.ckpt"
 gdown.download(id = id, output = output)
 
